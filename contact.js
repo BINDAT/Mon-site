89edbef4d1fc16f71326f5db63cfcf2af3c12d51
BINDAT <136336341+BINDAT@users.noreply.github.com>
2024-12-03 23:20:36 +0100

    Initial commit

---
+++ contact.js
@@ -0,0 +1,15 @@
+function github(){
+    location.href = 'https://github.com/BINDAT'
+    }
+
+function linkedin(){
+    location.href = 'https://www.linkedin.com/in/maxime-rigaud-604235291/'
+    }
+
+function mail(){
+    location.href = 'mailto:infs693@proton.me'
+    }
+
+function cv(){
+    location.href = 'CV.pdf'
+    }
