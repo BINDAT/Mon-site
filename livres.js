24df403f88e9a7b8da71d0912bb1e2d4e6780779
BINDAT <136336341+BINDAT@users.noreply.github.com>
2025-01-02 20:00:39 +0100

    Create livres.js

---
+++ livres.js
@@ -0,0 +1,12 @@
+function olivre(livre) {
+    switch(livre) 
+    {
+        case 'Python_pour_les_Nuls':
+            window.open('https://www.decitre.fr/livres/python-pour-les-nuls-9782412088838.html','_blank');
+            break;
+        
+        case 'Apprendre_à_programmer_en_C_pour_les_nuls': 
+            window.open('https://www.decitre.fr/livres/apprendre-a-programmer-en-c-pour-les-nuls-9782412066966.html')    
+            break;
+    }
+}
