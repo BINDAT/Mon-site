c78a7deae7f8c1b0861d0a67162012525491c0b7
BINDAT <136336341+BINDAT@users.noreply.github.com>
2025-01-02 21:17:17 +0100

    Update livres.js

--- livres.js
+++ livres.js
@@ -6,7 +6,11 @@ function olivre(livre) {
             break;
         
         case 'Apprendre_à_programmer_en_C_pour_les_nuls': 
-            window.open('https://www.decitre.fr/livres/apprendre-a-programmer-en-c-pour-les-nuls-9782412066966.html')    
+            window.open('https://www.decitre.fr/ebooks/apprendre-a-programmer-en-c-pour-les-nuls-9782412024140_9782412024140_3.html');
+            break;
+        
+        case 'Manuel_du_hacker':
+            window.open('https://www.amazon.fr/dp/B08JDXBLSZ?ref=ppx_yo2ov_dt_b_fed_asin_title');
             break;
     }
 }
