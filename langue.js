fcf0bc58b66f04d9d210e76c5efa9b81b3b51c72
BINDAT <136336341+BINDAT@users.noreply.github.com>
2025-01-08 23:37:29 +0100

    Create langue.js

---
+++ langue.js
@@ -0,0 +1,15 @@
+function english()
+{
+    document.getElementById("btrad").innerHTML = "français";
+    document.getElementById("english_titre_bloc_intro").innerHTML = "Rigaud Maxime <br> Apprendice coder";
+    document.getElementById("paragraphe_intro").innerHTML = "Apprentices in the world of computer programming <br> I would be delighted to make my knowledge available."
+    document.getElementById("sous_titre_activités").innerHTML = "My readings and occupations:";
+    document.getElementById("activité_langue").innerHTML = "Learning several languages on Duolingo";
+    document.getElementById("activité_informatique").innerHTML = "Learning programming language with Mimo, WildLearner and OpenClassRoom";
+    document.getElementById("livres").innerHTML = "My readings :";
+    document.getElementById("parrainage1").innerHTML = "Sponsorship link :";
+    document.getElementById("parrainage").innerHTML = "KVGPFM <br> Invitation code for the application <br> Bitcoin Mining";
+    document.getElementById("tradcontact").innerHTML = "My contacts :"
+    document.getElementById("tradsources").innerHTML = "My sources :"
+}
+
