e36c4feb2b7315480e56b1f6cf7e275de6f265b8
BINDAT <136336341+BINDAT@users.noreply.github.com>
2025-01-09 11:38:11 +0100

    Update langue.js

--- langue.js
+++ langue.js
@@ -1,9 +1,8 @@
 function english()
 {
-    document.getElementById("btrad").innerHTML = "français";
-    document.getElementById("english_titre_bloc_intro").innerHTML = "Rigaud Maxime <br> Apprendice coder";
+    document.getElementById("langue_titre_bloc_intro").innerHTML = "Rigaud Maxime <br> Apprendice coder";
     document.getElementById("paragraphe_intro").innerHTML = "Apprentices in the world of computer programming <br> I would be delighted to make my knowledge available."
-    document.getElementById("sous_titre_activités").innerHTML = "My readings and occupations:";
+    document.getElementById("sous_titre_activités").innerHTML = "My readings and occupations :";
     document.getElementById("activité_langue").innerHTML = "Learning several languages on Duolingo";
     document.getElementById("activité_informatique").innerHTML = "Learning programming language with Mimo, WildLearner and OpenClassRoom";
     document.getElementById("livres").innerHTML = "My readings :";
@@ -13,3 +12,15 @@ function english()
     document.getElementById("tradsources").innerHTML = "My sources :"
 }
 
+function français() {
+    document.getElementById("langue_titre_bloc_intro").innerHTML = "Rigaud Maxime <br> Apprentis Codeur";
+    document.getElementById("paragraphe_intro").innerHTML = "Apprentis dans le monde de la programmation informatique <br> je serait ravi de mettre à disposition mon savoir."
+    document.getElementById("sous_titre_activités").innerHTML = "Mes lectures & occupations :";
+    document.getElementById("activité_langue").innerHTML = "Apprentissage de plusieurs langues sur Duolingo";
+    document.getElementById("activité_informatique").innerHTML = "Apprentissage de langage de programmation avec Mimo, WildLearner et OpenClassRoom";
+    document.getElementById("livres").innerHTML = "Mes lectures :";
+    document.getElementById("parrainage1").innerHTML = "Lien de parrainage :";
+    document.getElementById("parrainage").innerHTML = "KVGPFM <br> code d'invitation de l'application <br> Bitcoin Mining";
+    document.getElementById("tradcontact").innerHTML = "Mes contacts :"
+    document.getElementById("tradsources").innerHTML = "Mes sources :"
+}
