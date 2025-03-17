c0afec759362898f7500f345922ca937067e635e
bindat <maximerigaud66@gmail.com>
2025-03-17 21:44:52 +0100

    minor update

--- script.js
+++ script.js
@@ -77,7 +77,7 @@ function olangue(langue) {
             document.getElementById("paragraphe_intro").innerHTML = "Apprentis dans le monde de la programmation informatique <br> je serait ravi de mettre à disposition mon savoir."
             document.getElementById("sous_titre_activités").innerHTML = "Mes lectures & occupations :";
             document.getElementById("activité_langue").innerHTML = "Apprentissage de plusieurs langues sur Duolingo";
-            document.getElementById("activité_informatique").innerHTML = "Apprentissage de langage de programmation avec Mimo, WildLearner et OpenClassRoom";
+            document.getElementById("activité_informatique").innerHTML = "Apprentissage de langage de programmation avec anciennement Mimo, WildLearner et OpenClassRoom";
             document.getElementById("livres").innerHTML = "Mes lectures :";
             document.getElementById("parrainage1").innerHTML = "Lien de parrainage :";
             document.getElementById("parrainage").innerHTML = "KVGPFM <br> code d'invitation de l'application <br> Bitcoin Mining";
@@ -91,7 +91,7 @@ function olangue(langue) {
             document.getElementById("paragraphe_intro").innerHTML = "Apprentices in the world of computer programming <br> I would be delighted to make my knowledge available."
             document.getElementById("sous_titre_activités").innerHTML = "My readings and occupations :";
             document.getElementById("activité_langue").innerHTML = "Learning several languages on Duolingo";
-            document.getElementById("activité_informatique").innerHTML = "Learning programming language with Mimo, WildLearner and OpenClassRoom";
+            document.getElementById("activité_informatique").innerHTML = "Learning programming language with previously Mimo, WildLearner and OpenClassRoom";
             document.getElementById("livres").innerHTML = "My readings :";
             document.getElementById("parrainage1").innerHTML = "Sponsorship link :";
             document.getElementById("parrainage").innerHTML = "KVGPFM <br> Invitation code for the application <br> Bitcoin Mining";
